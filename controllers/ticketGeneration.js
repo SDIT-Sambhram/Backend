a3773a5ceb4ade153765e56b0dcfc8eed7c929b2
shafaz <shafazabdulla@gmail.com>
2024-11-07 20:03:49 +0530

    ticket generation testing

--- controllers/ticketGeneration.js
+++ controllers/ticketGeneration.js
@@ -1,6 +1,9 @@
 import path from 'path';
 import { updateTicketImage } from '../helpers/imageUpdation.js';
 // import { uploadImageToS3 } from '../helpers/uploadImagetoS3.js';
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const generateTicket = async (participantId, name, phone, qr_code) => {
   try {
