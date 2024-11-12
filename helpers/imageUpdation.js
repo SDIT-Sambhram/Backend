9140bec2f7ee75f12541a0e129f402f7f22bad80
shafaz <shafazabdulla@gmail.com>
2024-11-12 21:46:13 +0530

    updated webhook to update all object

--- helpers/imageUpdation.js
+++ helpers/imageUpdation.js
@@ -12,13 +12,11 @@ export const updateTicketImage = async (participantId, name, phone, price, event
   try {
     // Path to the base ticket image
     const baseTicketPath = path.join(__dirname, `../images/tickets/${eventCount}.png`);
-
-    
+    console.log('Base ticket path:', baseTicketPath);
 
     // Generate the QR code base64 string using the provided data
     const qrCodeBase64 = await generateQRCode(participantId);
     const qrCodeBuffer = Buffer.from(qrCodeBase64, 'base64');
-
     console.log('QR code generated successfully.', qrCodeBuffer);
 
     // Ensure the base ticket image exists
@@ -26,26 +24,27 @@ export const updateTicketImage = async (participantId, name, phone, price, event
     if (!ticketExists) {
       throw new Error('Base ticket image not found at the specified path');
     }
+    console.log('Base ticket image exists.');
 
     // Create the updated image with participant details and QR code
     const updatedImageBuffer = await sharp(baseTicketPath)
       .composite([
         {
           input: Buffer.from(`
-            <svg width="300" height="825" xmlns="http://www.w3.org/2000/svg">
+            <svg width="300" height="825">
               <style>
                 .name { font-size: 18px; fill: #E4E3E3; font-family: 'Montserrat'; }
                 .phone { font-size: 18px; fill: #E4E3E3; font-family: 'Montserrat'; }
               </style>
-              <text x="15" y="460" class="name">Name: ${name}</text>
-              <text x="15" y="500" class="phone">Phone: ${phone}</text>
+              <text x="15" y="160" class="name">Name: ${name}</text>
+              <text x="15" y="300" class="phone">Phone: ${phone}</text>
             </svg>
           `, 'utf-8'),
           gravity: 'northwest',
         },
         {
           input: Buffer.from(`
-            <svg width="300" height="825" xmlns="http://www.w3.org/2000/svg">
+            <svg width="300" height="825">
               <style>
                 .eventCount { font-size: 18px; fill: #E4E3E3; font-family: 'Montserrat'; }
                 .price { font-size: 20px; fill: #E4E3E3; font-family: 'Montserrat'; }
