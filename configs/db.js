df9831feab170649e95d774a6d73bf664e2203d4
shafaz <shafazabdulla@gmail.com>
2024-11-13 12:01:01 +0530

    updated webhook to update all object

--- configs/db.js
+++ configs/db.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`Connected to MongoDB ${conn.connection.host}`.bgMagenta.white);
+        console.log(`Connected to MongoDB`);
         return conn;
     } catch (error) {
-        console.error(`Error in connecting MongoDB: ${error}`.bgRed.white);
+        console.error(`Error in connecting MongoDB:`);
         process.exit(1); // Exit process with failure
     }
 }
