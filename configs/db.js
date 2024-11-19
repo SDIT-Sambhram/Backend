e3969c227e7b81fac86527db643778c703061e08
shafaz <shafazabdulla@gmail.com>
2024-11-20 02:51:35 +0530

    mongodb pooling

--- configs/db.js
+++ configs/db.js
@@ -2,13 +2,20 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`Connected to MongoDB`);
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true, // Use the new MongoDB URL parser
+            useUnifiedTopology: true, // Enables connection pooling and monitoring
+            maxPoolSize: 20, // Set the maximum number of connections in the pool
+            serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds if server is unavailable
+            socketTimeoutMS: 45000, // Timeout for inactivity on a connection (45 seconds)
+        });
+
+        console.log(`Connected to MongoDB: ${conn.connection.host}`);
         return conn;
     } catch (error) {
-        console.error(`Error in connecting MongoDB:`);
+        console.error(`Error in connecting MongoDB: ${error.message}`);
         process.exit(1); // Exit process with failure
     }
-}
+};
 
 export default connectDB;
