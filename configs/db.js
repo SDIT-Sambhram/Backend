23d147ce757de2e3bd042a53b987e48fefe2fa0f
shafaz <shafazabdulla@gmail.com>
2024-11-20 10:49:17 +0530

    final commit

--- configs/db.js
+++ configs/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL, {
-            useUnifiedTopology: true, // Enables connection pooling and monitoring
             maxPoolSize: 20, // Set the maximum number of connections in the pool
             serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds if server is unavailable
             socketTimeoutMS: 45000, // Timeout for inactivity on a connection (45 seconds)
